dd57ddf951741cd9e26dde69eae2bd61da29fe4a
Peter Magenheimer <peter.magenheimer@gmail.com>
2012-10-18 19:59:38 -0600

    Add document ready to script.js.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
-$('.swipe').each(function() {
-    var galleryInner = $(this).children('.gallery-inner')[0];
-    var galleryControls = $(galleryInner).next('.gallery-controls');
-    var itemCount = $(galleryInner).find('li').length;
-    for (var i = itemCount - 1; i >= 0; i--) {
-        $(galleryControls).append('<span>&bull;</span>');
-    }
-    var bullets = $(galleryControls).children('span');
-    $(bullets[0]).addClass('on');
-
-    var slider = new Swipe(galleryInner, {
-        callback: function(e, pos) {
-            var i = bullets.length;
-            while (i--) {
-                bullets[i].className = ' ';
-            }
-            bullets[pos].className = 'on';
+$(function() {
+    $('.swipe').each(function() {
+        var galleryInner = $(this).children('.gallery-inner')[0];
+        var galleryControls = $(galleryInner).next('.gallery-controls');
+        var itemCount = $(galleryInner).find('li').length;
+        for (var i = itemCount - 1; i >= 0; i--) {
+            $(galleryControls).append('<span>&bull;</span>');
         }
-    });
+        var bullets = $(galleryControls).children('span');
+        $(bullets[0]).addClass('on');
 
-    galleryControls.find('.left').click(function() {
-        slider.prev();
-        return false;
-    });
+        var slider = new Swipe(galleryInner, {
+            callback: function(e, pos) {
+                var i = bullets.length;
+                while (i--) {
+                    bullets[i].className = ' ';
+                }
+                bullets[pos].className = 'on';
+            }
+        });
 
-    galleryControls.find('.right').click(function() {
-        slider.next();
-        return false;
+        galleryControls.find('.left').click(function() {
+            slider.prev();
+            return false;
+        });
+
+        galleryControls.find('.right').click(function() {
+            slider.next();
+            return false;
+        });
     });
 });
